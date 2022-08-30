a47b440dd3a8dd95c9078c8863088b4e26be0bce
William Mantilla <william.mantilla14@gmail.com>
2022-08-30 00:12:48 -0500

    Creación de clase Empresa

---
+++ Empresa.java
@@ -0,0 +1,46 @@
+public class Empresa {
+
+    private String nombreEmpresa;
+    private String direccion;
+    private String telefono;
+    private long nit;
+
+    // Getters and Setters
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public long getNit() {
+        return nit;
+    }
+    public void setNit(long nit) {
+        this.nit = nit;
+    }
+    //ToString
+    @Override
+    public String toString() {
+        return "Empresa [direccion=" + direccion + ", nit=" + nit + ", nombreEmpresa=" + nombreEmpresa + ", telefono="
+                + telefono + "]";
+    }
+    //Constructor
+    public Empresa(String nombreEmpresa, String direccion, String telefono, long nit) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+}
